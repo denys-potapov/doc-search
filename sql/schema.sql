d18e30d36fcd779dc59d3457b2573288688593bd
Denys Potapov <denys.potapov@gmail.com>
2022-12-13 05:21:08 +0200

    fix schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -7,5 +7,14 @@ CREATE TABLE IF NOT EXISTS public.documents
     id uuid NOT NULL DEFAULT gen_random_uuid(),
     text text COLLATE pg_catalog."default" NOT NULL DEFAULT ''::text,
     status character varying(8) COLLATE pg_catalog."default" NOT NULL DEFAULT 'PENDING'::character varying,
+    ts tsvector GENERATED ALWAYS AS (to_tsvector('simple'::regconfig, text)) STORED,
     CONSTRAINT documents_pkey PRIMARY KEY (id)
-)
+);
+
+-- Index: ts_idx
+
+-- DROP INDEX IF EXISTS public.ts_idx;
+
+CREATE INDEX IF NOT EXISTS ts_idx
+    ON public.documents USING gin
+    (ts);
